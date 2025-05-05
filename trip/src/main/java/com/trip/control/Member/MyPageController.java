91e524b51035c9bb99f13d96ab8d30854fd36513
dmsgml7476 <dmsgml7476@gmail.com>
2025-05-05 19:36:06 +0900

    0505 마이페이지 시작

---
+++ trip/src/main/java/com/trip/control/Member/MyPageController.java
@@ -0,0 +1,26 @@
+package com.trip.control.Member;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.trip.config.auth.CustomUserDetails;
+import com.trip.repository.Member.UserAlertSettingRepository;
+import com.trip.repository.Member.UserRepository;
+import com.trip.repository.Member.WebNotificationRepository;
+import com.trip.repository.Planner.TripPlanRepository;
+import com.trip.service.Member.RecommendStoryService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class MyPageController {
+
+	
+	@GetMapping("/mypage")
+	public String myPage(@AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
+		return "member/myPage";
+	}
+}
