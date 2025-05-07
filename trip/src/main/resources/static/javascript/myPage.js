5ce8b1fef4c654ac0507b51d97cb92d150acb2c9
dmsgml7476 <dmsgml7476@gmail.com>
2025-05-07 20:53:02 +0900

    0507 캘린더

--- trip/src/main/resources/static/javascript/myPage.js
+++ trip/src/main/resources/static/javascript/myPage.js
@@ -87,3 +87,60 @@ const moreBtn = document.getElementById('moreBtn');
 	
   });
 });
+
+window.onload = function () {
+        let today = new Date();
+        var nowY = today.getFullYear(); //현재 연도
+        var nowM = today.getMonth(); //현재 월
+        var nowD = today.getDate(); //현재 일
+
+        //연도, 월을 입력받지 않은 경우에는 현재 날짜의 연도, 월 정보를 사용함
+        y = nowY;
+        m = nowM;
+
+        //현재 월의 1일 정보
+        var theDate = new Date(y, m, 1);
+        var theDay = theDate.getDay();
+
+        //1월부터 12월까지 마지막 일을 배열로 저장
+        var last = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+        //윤년 계산
+        if ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0)
+          lastDate = last[1] = 29;
+
+        //현재 월의 마지막 일 정보
+        var lastDate = last[m]; //현재 월의 마지막이 몇일인지 구함
+
+        //현재 월의 달력에 필요한 행의 개수
+        var row = Math.ceil((theDay + lastDate) / 7);
+
+        let cal = document.getElementById("cal");
+
+        var draw = "<table class='calendar'>";
+        var dNum = 1;
+        for (var i = 1; i <= 6; i++) {
+          draw += "<tr>";
+          for (var k = 0; k < 7; k++) {
+            if ((i == 1 && k < theDay) || dNum > lastDate) {
+              draw += "<td class='day-box'> &nbsp; </td>";
+            } else {
+              //오늘 날짜
+              if (dNum === nowD) {
+                draw += "<td class='day-box'>" + dNum + "</td>";
+              }
+              //오늘이 아닌 날짜
+              else {
+                draw += "<td class='day-box'>" + dNum + "</td>";
+              }
+              //일 증가
+              dNum++;
+            }
+          }
+          draw += "</tr>";
+        }
+
+        draw += "</table>";
+
+        cal.innerHTML = draw;
+      };
