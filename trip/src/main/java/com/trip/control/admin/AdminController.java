d0fb4137fe91065dbc1bd13bcec60f363607acab
dmsgml7476 <dmsgml7476@gmail.com>
2025-05-09 23:54:15 +0900

    0509 저장안하고 온거 복구/관리자페이지 시작/ 여행지 등록하는중

---
+++ trip/src/main/java/com/trip/control/admin/AdminController.java
@@ -0,0 +1,24 @@
+package com.trip.control.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/admin")
+public class AdminController {
+	
+	@GetMapping("/main")
+	public String adminMainPage() {
+		return "admin/index";
+	}
+	
+	@GetMapping("/csMgmt")
+	public String csMgmtPage() {
+		return "admin/page/csMgmt";
+	}
+	
+}
