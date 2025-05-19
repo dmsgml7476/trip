e13ecfd35ee1aa52d6a3353eb81aff061567b3e9
sjkfeel <layum@naver.com>
2025-05-19 15:56:30 +0900

    오후 4시 수정본

--- trip/src/main/resources/static/javascript/placeListOption.js
+++ trip/src/main/resources/static/javascript/placeListOption.js
@@ -3,20 +3,20 @@ const typeSelect = document.getElementById("typeSelect");
 const placeSelect = document.getElementById("placeSelect");
 
 typeSelect.addEventListener('change', () => {
-	const selectedType = typeSelect.value;
-	
-	
-	placeSelect.innerHTML='<option value="">장소 선택</option>';
-	
-	placeInfo.forEach(placeInfo=>{
-		if(placeInfo.categoryId===parseInt(selectedType)){
-			const option = document.createElement('option');
-			option.value = placeInfo.placeId;
-			option.textContent=placeInfo.placeName;
-			placeSelect.appendChild(option);
-			
-		}
-	});
+   const selectedType = typeSelect.value;
+   
+   
+   placeSelect.innerHTML='<option value="">장소 선택</option>';
+   
+   placeInfo.forEach(placeInfo=>{
+      if(placeInfo.categoryId===parseInt(selectedType)){
+         const option = document.createElement('option');
+         option.value = placeInfo.placeId;
+         option.textContent=placeInfo.placeName;
+         placeSelect.appendChild(option);
+         
+      }
+   });
 }); 
 
 function saveSelection() {
